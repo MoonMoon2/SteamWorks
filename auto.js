da6ab88e9fca96c7dd9e99fb57a285c31cc37e3f
MoonMoon2 <tate@tatesworld.com>
2017-01-15 17:33:52 -0500

    Setup Work
    —auto.js—
    - Necessary NetworkTables things added
    —style.css—
    - Considering using row classes, as in VictiScout, to split up data and
    buttons

---
+++ auto.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+// Sets function to be called on NetworkTables connect. Commented out because it's usually not necessary.
+// NetworkTables.addWsConnectionListener(onNetworkTablesConnection, true);
+// Sets function to be called when robot dis/connects
+NetworkTables.addRobotConnectionListener(onRobotConnection, true);
+// Sets function to be called when any NetworkTables key/value changes
+NetworkTables.addGlobalListener(onValueChanged, true);
+
+function onRobotConnection(connected) {
+	var state = connected ? 'Robot connected!' : 'Robot disconnected.';
+	console.log(state);
+	ui.robotState.innerHTML = state;
+}
